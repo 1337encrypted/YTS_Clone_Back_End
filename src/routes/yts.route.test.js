1ad51668bafc327e1f0921cf40a33b1fed0e8a91
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2022-12-30 19:52:16 +0530

    fix: endpoint typo and disable logging

--- src/routes/yts.route.test.js
+++ src/routes/yts.route.test.js
@@ -52,42 +52,34 @@ const listMoviesTestPayload = [
         urls: [`${HOST}/list_movies?with_rt_ratings=sometext`],
         errcode: "ERR_RTR",
     },
-    {
-        urls: [`${HOST}/list_movies`],
-        errcode: null,
-    },
 ];
 const movieDetailsTestPayload = [
     {
-        urls: [`${HOST}/movie_details.json`],
+        urls: [`${HOST}/movie_details`],
         errcode: "ERR_NOID",
     },
     {
         urls: [
-            `${HOST}/movie_details.json?movie_id=`,
-            `${HOST}/movie_details.json?imdb_id=`,
-            `${HOST}/movie_details.json?movie_id=sometext`,
-            `${HOST}/movie_details.json?imdb_id=sometext`,
+            `${HOST}/movie_details?movie_id=`,
+            `${HOST}/movie_details?imdb_id=`,
+            `${HOST}/movie_details?movie_id=sometext`,
+            `${HOST}/movie_details?imdb_id=sometext`,
         ],
         errcode: "ERR_INVID",
     },
     {
         urls: [
-            `${HOST}/movie_details.json?with_images=`,
-            `${HOST}/movie_details.json?with_images=sometext`,
+            `${HOST}/movie_details?movie_id=10&with_images=`,
+            `${HOST}/movie_details?movie_id=10&with_images=sometext`,
         ],
         errcode: "ERR_WIE",
     },
     {
         urls: [
-            `${HOST}/movie_details.json?with_cast=`,
-            `${HOST}/movie_details.json?with_cast=sometext`,
+            `${HOST}/movie_details?movie_id=10&with_cast=`,
+            `${HOST}/movie_details?movie_id=10&with_cast=sometext`,
         ],
         errcode: "ERR_WCE",
-    },
-    {
-        urls: [`${HOST}/movie_details.json?movie_id=10`],
-        errcode: null,
     },
 ];
 
@@ -100,11 +92,8 @@ afterAll(() => httpApp.close());
 test("Validates query parameters for `/list_movies` endpoint", async () => {
     // eslint-disable-next-line no-restricted-syntax, node/no-unsupported-features/es-syntax
     for await (const testPayload of listMoviesTestPayload) {
-        console.log("Testing for error", testPayload.errcode);
         // eslint-disable-next-line no-restricted-syntax, no-await-in-loop, node/no-unsupported-features/es-syntax
         for await (const url of testPayload.urls) {
-            console.log("-", url);
-
             const response = await fetch(url);
             const json = await response.json();
 
@@ -122,11 +111,8 @@ test("Validates query parameters for `/list_movies` endpoint", async () => {
 test("Validates query parameters for `/movie_details` endpoint", async () => {
     // eslint-disable-next-line no-restricted-syntax, node/no-unsupported-features/es-syntax
     for await (const testPayload of movieDetailsTestPayload) {
-        console.log("Testing for error", testPayload.errcode);
         // eslint-disable-next-line no-restricted-syntax, no-await-in-loop, node/no-unsupported-features/es-syntax
         for await (const url of testPayload.urls) {
-            console.log("-", url);
-
             const response = await fetch(url);
             const json = await response.json();
 
