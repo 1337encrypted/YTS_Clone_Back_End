3f9dac97965270b4232679771ff7563ddc18d20e
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-01-15 21:06:07 +0530

    feat(utils): common utils

---
+++ src/utils/utils.js
@@ -0,0 +1,39 @@
+// Setup
+require("dotenv").config();
+
+// Imports
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Constants
+const BCRYPT_SALTROUNDS = 10;
+const { JWT_SECRET } = process.env;
+
+// Body
+async function bcryptHash(s) {
+    return bcrypt.hash(s, BCRYPT_SALTROUNDS);
+}
+
+async function bcryptCompare(hash, s) {
+    return bcrypt.compare(s, hash);
+}
+
+async function jwtSign(obj, expires = undefined) {
+    return jwt.sign(obj, JWT_SECRET, !expires ? undefined : { expiresIn: expires });
+}
+
+async function jwtDecoded(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if (err) reject(err);
+            return resolve(decoded);
+        });
+    });
+}
+
+module.exports = {
+    bcryptHash,
+    bcryptCompare,
+    jwtSign,
+    jwtDecoded,
+};
