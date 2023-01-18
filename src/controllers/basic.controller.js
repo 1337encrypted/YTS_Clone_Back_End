4c8c77c44f7ededbba4a49441bb0b7aae814fac6
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-01-18 21:13:40 +0530

    fix: broken api response

--- src/controllers/basic.controller.js
+++ src/controllers/basic.controller.js
@@ -1,5 +1,9 @@
+// Imports
+const Response = require("../models/standard.response.model");
+
+// Body
 async function completeRequest(req, res) {
-    return res.status(200).send(res.body);
+    return res.status(200).send(Response(false, res.body));
 }
 
 module.exports = {
